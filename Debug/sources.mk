29f58665fc33d7dacd6edae1d487c583e62ce703
Chouxmimouli <chaxmouxmiloux@proton.me>
2024-03-29 22:21:55 +0100

    Trigger wheel and usb programming

--- Debug/sources.mk
+++ Debug/sources.mk
@@ -19,10 +19,15 @@ MAP_FILES :=
 S_DEPS := 
 S_UPPER_DEPS := 
 C_DEPS := 
+OBJCOPY_BIN := 
 
 # Every subdirectory with source files must be described here
 SUBDIRS := \
 Core/Src \
 Core/Startup \
 Drivers/STM32F4xx_HAL_Driver/Src \
+Middlewares/ST/STM32_USB_Device_Library/Class/CDC/Src \
+Middlewares/ST/STM32_USB_Device_Library/Core/Src \
+USB_DEVICE/App \
+USB_DEVICE/Target \
 
